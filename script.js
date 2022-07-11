47deaba9301553cf3c55ac09f254702e24dd9ab7
fmrc <fatih.mehmed.cin@gmail.com>
2022-07-11 19:45:16 +0200

    feat: add painting functionalitz

--- script.js
+++ script.js
@@ -3,6 +3,14 @@ const btn16 = document.querySelector('[data-btn="16"]');
 const btn32 = document.querySelector('[data-btn="32"]');
 const btn64 = document.querySelector('[data-btn="64"]');
 
+let mousePressed = false;
+document.body.addEventListener("mousedown", () => {
+  mousePressed = true;
+});
+document.body.addEventListener("mouseup", () => {
+  mousePressed = false;
+});
+
 function buildGrid(gridSize) {
   while (container.firstChild) {
     container.removeChild(container.firstChild);
@@ -11,9 +19,17 @@ function buildGrid(gridSize) {
   container.style.gridTemplateRows = `repeat(${gridSize}, 1fr)`;
   for (let i = 0; i < gridSize; i++) {
     for (let j = 0; j < gridSize; j++) {
-      let cell = document.createElement("div");
+      let cell = document.createElement("a");
       cell.classList.add("cell");
       container.appendChild(cell);
+      cell.addEventListener("dragstart", () => {
+        return false;
+      });
+      cell.addEventListener("mouseover", () => {
+        if (mousePressed) {
+          cell.style.backgroundColor = "black";
+        }
+      });
     }
   }
 }
