a90a2e3f6dac946a2771009f9cf8944013f3c899
fmrc <fatih.mehmed.cin@gmail.com>
2022-07-11 18:25:08 +0200

    fix: remove every cell

--- script.js
+++ script.js
@@ -4,14 +4,13 @@ const btn32 = document.querySelector('[data-btn="32"]');
 const btn64 = document.querySelector('[data-btn="64"]');
 
 function buildGrid(gridSize) {
-  let cells = container.childNodes;
-  cells.forEach((cell) => {
-    cell.remove();
-  });
+  while (container.firstChild) {
+    container.removeChild(container.firstChild);
+  }
   container.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
   container.style.gridTemplateRows = `repeat(${gridSize}, 1fr)`;
   for (let i = 0; i < gridSize; i++) {
-    for (let i = 0; i < gridSize; i++) {
+    for (let j = 0; j < gridSize; j++) {
       let cell = document.createElement("div");
       cell.classList.add("cell");
       container.appendChild(cell);
