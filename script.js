ab65adb17bf5693c86dd3a760184845b97d99b9c
fmrc <fatih.mehmed.cin@gmail.com>
2022-07-14 12:19:29 +0200

    refactor: make cell color named function

--- script.js
+++ script.js
@@ -4,12 +4,17 @@ const btn32 = document.querySelector('[data-btn="32"]');
 const btn64 = document.querySelector('[data-btn="64"]');
 
 let mousePressed = false;
-document.body.addEventListener("mousedown", () => {
+document.addEventListener("mousedown", () => {
   mousePressed = true;
 });
-document.body.addEventListener("mouseup", () => {
+document.addEventListener("mouseup", () => {
   mousePressed = false;
 });
+
+function colorCell(e) {
+  if (e.type !== "mousedown" && !mousePressed) return;
+  e.target.style.backgroundColor = "black";
+}
 
 function buildGrid(gridSize) {
   while (container.firstChild) {
@@ -19,17 +24,12 @@ function buildGrid(gridSize) {
   container.style.gridTemplateRows = `repeat(${gridSize}, 1fr)`;
   for (let i = 0; i < gridSize; i++) {
     for (let j = 0; j < gridSize; j++) {
-      let cell = document.createElement("a");
+      let cell = document.createElement("div");
       cell.classList.add("cell");
+      cell.setAttribute("draggable", "false");
       container.appendChild(cell);
-      cell.addEventListener("dragstart", () => {
-        return false;
-      });
-      cell.addEventListener("mouseover", () => {
-        if (mousePressed) {
-          cell.style.backgroundColor = "black";
-        }
-      });
+      cell.addEventListener("mousedown", colorCell);
+      cell.addEventListener("mouseover", colorCell);
     }
   }
 }
